17c03153d2bdf2e5398f9223c0445e5d49b64200
maxixking66 <maxixking66@gamil.com>
2025-12-25 10:48:56 +0330

    add persistence.xml and get EntityManager from Emf

---
+++ src/main/java/ir/maktabsharif/jpa/JpaApplication.java
@@ -0,0 +1,18 @@
+package ir.maktabsharif.jpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaApplication {
+
+    static void main() {
+        try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("default")) {
+
+            try (EntityManager entityManager = emf.createEntityManager()) {
+
+            }
+
+        }
+    }
+}
