4f568f81b97696ff8c01537b25a2a434b25c14e4
maxixking66 <maxixking66@gamil.com>
2026-02-06 09:57:36 +0330

    add thread safe feature for certain chqId

--- src/main/java/ir/maktabsharif/jpa/JpaApplication.java
+++ src/main/java/ir/maktabsharif/jpa/JpaApplication.java
@@ -1,75 +1,36 @@
 package ir.maktabsharif.jpa;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JpaApplication {
 
     static final Object MONITOR = new Object();
 
+    static Map<Long, Object> lockMap = new HashMap<>();
+
     static void main() throws InterruptedException {
 
-        Signal signal = new Signal();
-
-
-        new Thread(
-                () -> {
-                    try {
-                        signal.doWait();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-        ).start();
-
-
-        new Thread(
-                () -> {
-                    try {
-                        signal.doWait();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-        ).start();
-
-        new Thread(
-                () -> {
-                    try {
-                        Thread.sleep(2000);
-                        signal.doNotifyAll();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-        ).start();
-
-        System.out.println("end of main");
     }
 
-    static class Signal {
-
-        public void doWait() throws InterruptedException {
-            synchronized (MONITOR) {
-                System.out.printf("Thread %s before wait \n", Thread.currentThread().getName());
-                MONITOR.wait();
-                System.out.printf("Thread %s after wait \n", Thread.currentThread().getName());
-            }
+    static void block(Long chqId) {
+        Object lock = lockMap.computeIfAbsent(chqId, _ -> new Object());
+        synchronized (lock) {
+            System.out.println("block logic");
         }
+    }
 
-        public void doNotify() throws InterruptedException {
-            synchronized (MONITOR) {
-                System.out.printf("Thread %s before notify \n", Thread.currentThread().getName());
-                MONITOR.notify();
-                System.out.printf("Thread %s after notify \n", Thread.currentThread().getName());
-
-            }
+    static void unblock(Long chqId) {
+        Object lock = lockMap.computeIfAbsent(chqId, _ -> new Object());
+        synchronized (lock) {
+            System.out.println("unblock logic");
         }
+    }
 
-        public void doNotifyAll() throws InterruptedException {
-            synchronized (MONITOR) {
-                System.out.printf("Thread %s before notifyAll \n", Thread.currentThread().getName());
-                MONITOR.notifyAll();
-                System.out.printf("Thread %s after notifyAll \n", Thread.currentThread().getName());
-
-            }
+    static void approve(Long chqId) {
+        Object lock = lockMap.computeIfAbsent(chqId, _ -> new Object());
+        synchronized (lock) {
+            System.out.println("approve logic");
         }
     }
 
