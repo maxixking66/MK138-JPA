7b46f34abfeab4f9004df9a8fae9d66d96ad089e
maxixking66 <maxixking66@gamil.com>
2026-02-06 11:27:58 +0330

    add question code

--- src/main/java/ir/maktabsharif/jpa/JpaApplication.java
+++ src/main/java/ir/maktabsharif/jpa/JpaApplication.java
@@ -1,30 +1,56 @@
 package ir.maktabsharif.jpa;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
 public class JpaApplication {
 
-    static void main() throws InterruptedException, ExecutionException {
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
-        Future<String> submit = executorService.submit(
-                () -> {
-                    System.out.println("start");
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    System.out.println("end");
-                    return "mohsen";
-                }
-        );
-        String result = submit.get();
-        System.out.println("end of main with result: " + result);
-        executorService.shutdown();
+    static void main() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        getCustomerInformation("002112121");
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
     }
 
+    static CustomerInformation getCustomerInformation(String accountNumber) throws InterruptedException {
+        CustomerInformation customerInformation = new CustomerInformation();
+        fetchPersonalInformation(customerInformation, accountNumber);
+        fetchBalanceInformation(customerInformation, accountNumber);
+        fetchAccountInformation(customerInformation, accountNumber);
+        return customerInformation;
+    }
 
+    static void fetchPersonalInformation(CustomerInformation information,
+                                         String accountNumber) throws InterruptedException {
+        Thread.sleep(500);
+    }
+
+    static void fetchBalanceInformation(CustomerInformation information,
+                                        String accountNumber) throws InterruptedException {
+        Thread.sleep(1500);
+    }
+
+    static void fetchAccountInformation(CustomerInformation information,
+                                        String accountNumber) throws InterruptedException {
+        Thread.sleep(100);
+    }
+
+}
+
+class CustomerInformation {
+    PersonalInformation personal;
+    BalanceInformation balance;
+    AccountInformation account;
+}
+
+class PersonalInformation {
+    String firstName;
+    String lastName;
+    String nationalCode;
+}
+
+class BalanceInformation {
+    long balance;
+}
+
+class AccountInformation {
+    String accountNumber;
+    String branch;
 }
